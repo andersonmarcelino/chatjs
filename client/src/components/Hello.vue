547e5dc7777b83e6556cd71be4f04a299845a114
Anderson Marcelino <anderson44444@gmail.com>
2017-09-22 03:25:27 -0300

    chat Complete

---
+++ client/src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <div class="header">
+      <label>Nickname:</label>
+      <input v-model="nickname">
+    </div>
+    <div class="chat">
+      <div v-for="message in messagelist" class="chat-message">
+        {{message}}
+      </div>
+    </div>
+    <div class="message">
+      <input v-model="message" v-on:keyup.enter="send">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    var that = this;
+    window.socket.on('message', function(message){
+      that.messagelist.push(message);
+      console.log(that.messagelist);
+    });
+    return {
+      nickname: "",
+      message: "",
+      messagelist: [],
+    }
+  },
+  methods:{
+    send: function () {
+      window.socket.emit('message',{
+        message: this.message,
+        nickname: this.nickname
+      });
+      this.message = '';
+    }
+  }
+}
+</script>
+<style src='./Hello.css'></style>
--- server/index.js
+++ server/index.js
@@ -1,3 +1,24 @@
-var Rx = require('rx');
-var RxNode = require('rx-node');
+var Rx = require('rxjs/Rx');
 var io = require('socket.io')(3001);
+
+var observer;
+var source = Rx.Observable.create((ob) => {
+  observer = ob;
+});
+
+var pub = source.publish();
+
+pub.connect();
+
+io.on('connection', (socket) => {
+  pub.subscribe({
+    next: (data) => {
+      socket.emit('message',data.nickname + ': ' + data.message);
+    },
+    error: (error) => {},
+    complete: () => {}});
+
+  socket.on('message', (data) => {
+    observer.next(data);
+  })
+});
